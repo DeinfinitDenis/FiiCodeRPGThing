bb4f28e6618f032874b77450fd32b855b09d9726
DreamsGL <luncanu.gabriel@gmail.com>
2024-02-18 23:51:22 +0200

    18 feb ora 11:50 save
    salvat dupa ce ati iesit voi
    -am terminat arcul si sword slashul

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
+    //player movement
     private Rigidbody2D mainplayer;
     public float moveHorizontal, moveVertical;
-    public float speed = 5f;
+    public float speed = 10f;
 
+    //slash skill
     public bool isSlash = false;
-    public BoxCollider2D slashBox;
-    public SpriteRenderer slashSprite;
+    public bool isArrow = false;
+    public GameObject slash;
+
+    //Bow Weapon
+    public GameObject arrow;
+    public float arrowSpeed = 40f;
+
+    public int weaponslot = 1;
+    public int dx, dy;
 
     void Start(){
         mainplayer = gameObject.GetComponent<Rigidbody2D>();
-        slashBox.enabled = false;
-        slashSprite.enabled = false;
+        slash.SetActive(false);
+        mainplayer.transform.eulerAngles = new Vector3(0f, 0f, 180f);
+        dx = 0;
+        dy = -1;
     }
 
     
@@ -23,27 +34,91 @@ public class PlayerActions : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
+        if(!isSlash)
+            DirectionCode();
+        
         if(Input.GetMouseButtonDown(0)){
-            if(!isSlash)
-                StartCoroutine(SlashDelay());
+            if(weaponslot == 1){
+                if(!isSlash)
+                    SlashCode();    
+            }
+            else{
+                if(!isArrow){
+                    ArrowCode();
+                }
+                    
+            }
         }
+
+        if(Input.GetKey(KeyCode.Alpha1))
+            weaponslot = 1;
+        else if(Input.GetKey(KeyCode.Alpha2))
+            weaponslot = 2;
 
     }
 
     void FixedUpdate(){
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         movement.Normalize();
-        mainplayer.velocity = movement * speed;
+
+        if(!isSlash){
+            mainplayer.velocity = movement * speed;
+        }
+        else mainplayer.velocity = new Vector2(0f, 0f);
     }
 
+    void DirectionCode(){
+        if(Input.GetKey(KeyCode.A)){
+            mainplayer.transform.eulerAngles = new Vector3(0f, 0f, -90f);
+            dx = -1;
+            dy = 0;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            mainplayer.transform.eulerAngles = new Vector3(0f, 0f, 90f);
+            dx = 1;
+            dy = 0;
+        }
+        if(Input.GetKey(KeyCode.W)){
+            mainplayer.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            dx = 0;
+            dy = 1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            mainplayer.transform.eulerAngles = new Vector3(0f, 0f, 180f);
+            dx = 0;
+            dy = -1;
+        }
+    }
+
+    //Slash Skill
+    void SlashCode(){
+        StartCoroutine(SlashDelay());
+        slash.transform.position = new Vector2(mainplayer.transform.position.x + dx, mainplayer.transform.position.y + dy);
+        slash.transform.eulerAngles = new Vector3(0f, 0f, mainplayer.rotation);
+        
+    }
+    
     IEnumerator SlashDelay(){
         isSlash = true;
-        slashBox.enabled = true;
-        slashSprite.enabled = true;
+        slash.SetActive(true);
         yield return new WaitForSeconds(0.5f);
+        slash.SetActive(false);
         isSlash = false;
-        slashBox.enabled = false;
-        slashSprite.enabled = false;
+
     }
 
+    //Arrow
+    void ArrowCode(){
+        Vector2 direction = new Vector2(dx, dy);
+        var harrow = Instantiate(arrow, mainplayer.transform.position, mainplayer.transform.rotation);
+        harrow.GetComponent<Rigidbody2D>().velocity = direction * arrowSpeed;
+        StartCoroutine(ArrowDelay());
+        Destroy(harrow, 0.7f);
+    }
+
+    IEnumerator ArrowDelay(){
+        isArrow = true;
+        yield return new WaitForSeconds(0.7f);
+        isArrow = false;
+    }
 }
