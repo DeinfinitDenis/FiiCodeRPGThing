6bdb39f678012241a00e1c42fb177e9030314cb2
DreamsGL <luncanu.gabriel@gmail.com>
2024-02-21 17:47:54 +0200

    21 feb ora 5:47
    -mai multe chestii

---
+++ Assets/Scripts/WorldTransitScripts/Central Field.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralField : MonoBehaviour
+{
+    public GameObject player;
+    public Animator playerAnim;
+
+    void Start()
+    {
+        if(AwakeningZone.lastScene == 1){
+            player.transform.position = new Vector2(7.7f, -1.9f);
+            playerAnim.SetFloat("direction mem x", -1f);
+        }
+
+        AwakeningZone.lastScene = 2;
+    }
+
+}
