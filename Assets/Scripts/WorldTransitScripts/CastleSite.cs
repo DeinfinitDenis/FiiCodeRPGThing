041961aa1de9175bf33b6c24ec75e32672926a2e
DreamsGL <luncanu.gabriel@gmail.com>
2024-03-04 22:39:14 +0200

    yes
    yes

--- Assets/Scripts/WorldTransitScripts/CastleSite.cs
+++ Assets/Scripts/WorldTransitScripts/CastleSite.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CastleSite : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
+    public Animator playerAnim;
+    public GameObject player;
+    
     void Start()
     {
-        
-    }
+        if(AwakeningZone.lastScene == "Central Field"){
+            player.transform.position = new Vector2(0f, 0f);
+            playerAnim.SetFloat("direction mem x", 0f);
+            playerAnim.SetFloat("direction mem y", 1f);
+        }
+        if(AwakeningZone.lastScene == "SnowZone"){}
+        if(AwakeningZone.lastScene == "Canyon"){}
+        if(AwakeningZone.lastScene == "Village"){}
+        if(AwakeningZone.lastScene == "Other"){}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+         AwakeningZone.lastScene = "Castle Field";
     }
+    
 }
--- Assets/Scripts/WorldTransitScripts/Central Field.cs
+++ Assets/Scripts/WorldTransitScripts/Central Field.cs
@@ -21,6 +21,12 @@ public class CentralField : MonoBehaviour
             playerAnim.SetFloat("direction mem y", 0f);
         }
 
+        else if(AwakeningZone.lastScene == "Castle Field"){
+            player.transform.position = new Vector2(14f, 83.4f);
+            playerAnim.SetFloat("direction mem x", 0f);
+            playerAnim.SetFloat("direction mem y", -1f);
+        }
+
         AwakeningZone.lastScene = "Central Field";
     }
 
